41e1621fd4b58c7887b399b58e9a3a117c6d30b0
Mawusi Caleb Elolo <cemawusi@st.ug.edu.gh>
2024-06-26 17:09:56 +0000

    Completed home screen

--- Assignment5/screens/homeScreen.js
+++ Assignment5/screens/homeScreen.js
@@ -1,12 +1,185 @@
-// screens/HomeScreen.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import homeStyles from '../styles/homePageStyles'
+import homePageStyles from '../styles/homePageStyles';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView style={homeStyles.container}>
+        
+      
+        <View style={homeStyles.header}>
+            <View style={homeStyles.profileImageContainer}>
+                <Image source={require('../assets/profile.png')}/>
+            </View>
+        
+            <View style={homeStyles.welcomeMessageContainer}>
+                <Text style={homeStyles.welcomeText}>Welcome back,</Text>
+                <Text style={homeStyles.name}>Eric Atsu</Text>
+            </View>
+        
+            <View style={homeStyles.searchIconContainer}>
+                <Ionicons name='search-outline' size={30} style={homeStyles.searchIcon}/>
+            </View>
+      
+        </View>
+
+        <View style={homeStyles.cardContainer}>
+            <Image source={require('../assets/Card.png')}/>
+        </View>
+
+        <View style={homeStyles.actionsContainer}>
+            <TouchableOpacity style={homeStyles.actionButtonContainer}>
+                <View style={homeStyles.actionButton}>
+                    <Ionicons name='arrow-up' size={30}/>
+                </View>
+                <View style={homeStyles.actionText}>
+                    <Text>Sent</Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={homeStyles.actionButtonContainer}>
+                <View style={homeStyles.actionButton}>
+                    <Ionicons name='arrow-down' size={30}/>
+                </View>
+                <View style={homeStyles.actionText}>
+                    <Text>Receive</Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={homeStyles.actionButtonContainer}>
+                <View style={homeStyles.actionButton}>
+                    <Image source={require('../assets/loan.png')}/>
+                </View>
+                <View style={homeStyles.actionText}>
+                    <Text>Loan</Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={homeStyles.actionButtonContainer}>
+                <View style={homeStyles.actionButton}>
+                    <Ionicons name='cloud-upload-outline' size={30}/>
+                </View>
+                <View style={homeStyles.actionText}>
+                    <Text>Topup</Text>
+                </View>
+            </TouchableOpacity>
+
+        </View>
+
+        <View style={homeStyles.transactionsContainer}>
+            <View style={homeStyles.transctionHeader}>
+                <Text style={homeStyles.transactionHeaderText}>Transaction</Text>
+                <Text style={homeStyles.sellAllText}>Sell All</Text>
+            </View>
+            <View style={homeStyles.transaction}>
+                <View style={homeStyles.companyDetails}>
+                    <View style={homeStyles.transactionLogoContainer}>
+                        <Image source={require('../assets/apple.png')} style={homePageStyles.transactionLogo}/>
+                    </View>
+
+                    <View style={homeStyles.transactionTextContainer}>
+                        <Text style={homeStyles.transactionText}>Apple Store</Text>
+                        <Text style={homeStyles.transactionSubText}>Entertainment</Text>
+                    </View>
+                </View>
+
+                <View style={homeStyles.debit_creditContainer}>
+                        <Text style={homeStyles.debitAmount}>-$5,99</Text>
+                </View>
+            </View>
+
+            <View style={homeStyles.transaction}>
+                <View style={homeStyles.companyDetails}>
+                    <View style={homeStyles.transactionLogoContainer}>
+                        <Image source={require('../assets/spotify.png')}/>
+                    </View>
+
+                    <View style={homeStyles.transactionTextContainer}>
+                        <Text style={homeStyles.transactionText}>Spotify</Text>
+                        <Text style={homeStyles.transactionSubText}>Music</Text>
+                    </View>
+                </View>
+
+                <View style={homeStyles.debit_creditContainer}>
+                        <Text style={homeStyles.debitAmount}>-$12,99</Text>
+                </View>
+            </View>
+
+            <View style={homeStyles.transaction}>
+                <View style={homeStyles.companyDetails}>
+                    <View style={homeStyles.transactionLogoContainer}>
+                        <Image source={require('../assets/moneyTransfer.png')}/>
+                    </View>
+
+                    <View style={homeStyles.transactionTextContainer}>
+                        <Text style={homeStyles.transactionText}>Money Transfer</Text>
+                        <Text style={homeStyles.transactionSubText}>Transaction</Text>
+                    </View>
+                </View>
+
+                <View style={homeStyles.debit_creditContainer}>
+                        <Text style={homeStyles.creditAmount}>$300</Text>
+                </View>
+            </View>
+
+            <View style={homeStyles.transaction}>
+                <View style={homeStyles.companyDetails}>
+                    <View style={homeStyles.transactionLogoContainer}>
+                        <Image source={require('../assets/grocery.png')}/>
+                    </View>
+
+                    <View style={homeStyles.transactionTextContainer}>
+                        <Text style={homeStyles.transactionText}>Grocery</Text>
+                        <Text style={homeStyles.transactionSubText}>Food</Text>
+                    </View>
+                </View>
+
+                <View style={homeStyles.debit_creditContainer}>
+                        <Text style={homeStyles.debitAmount}>-$88</Text>
+                </View>
+            </View>
+
+            <View style={homeStyles.transaction}>
+                <View style={homeStyles.companyDetails}>
+                    <View style={homeStyles.transactionLogoContainer}>
+                        <Image source={require('../assets/netflix.png')} style={homePageStyles.transactionLogo}/>
+                    </View>
+
+                    <View style={homeStyles.transactionTextContainer}>
+                        <Text style={homeStyles.transactionText}>Netflix</Text>
+                        <Text style={homeStyles.transactionSubText}>Entertainment</Text>
+                    </View>
+                </View>
+
+                <View style={homeStyles.debit_creditContainer}>
+                        <Text style={homeStyles.debitAmount}>-$29,99</Text>
+                </View>
+            </View>
+
+            <View style={homeStyles.transaction}>
+                <View style={homeStyles.companyDetails}>
+                    <View style={homeStyles.transactionLogoContainer}>
+                        <Image source={require('../assets/mtn.png')} style={homePageStyles.transactionLogo}/>
+                    </View>
+
+                    <View style={homeStyles.transactionTextContainer}>
+                        <Text style={homeStyles.transactionText}>MTN</Text>
+                        <Text style={homeStyles.transactionSubText}>Data Plan</Text>
+                    </View>
+                </View>
+
+                <View style={homeStyles.debit_creditContainer}>
+                        <Text style={homeStyles.debitAmount}>-$25,99</Text>
+                </View>
+            </View>
+        </View>
+
+       
+
+    
+    </ScrollView>
   );
 }
 
--- Assignment5/styles/homePageStyles.js
+++ Assignment5/styles/homePageStyles.js
@@ -4,7 +4,138 @@ export default homeStyles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
+      padding: 10,
+    },
+    header: {
+      padding: 5,
+      flex: 1,
+      alignItems:'center',
+      flexDirection: 'row',
+      backgroundColor: '#ffffff',
+    },
+    profileImageContainer: {
+      height: 50,
+      width: 50,
+      objectFit: 'contain',
+      alignSelf: 'center',
+    },
+    welcomeMessageContainer: {
+      flexDirection: 'column',
+      paddingHorizontal: 20,
+      alignItems: 'flex-start',
+    },
+    welcomeText: {
+      fontSize: 20,
+      color: '#787e87',
+      marginLeft: 'auto',
+    },
+    name: {
+      fontSize: 30,
+    },
+    searchIconContainer: {
+      height: 50,
+      width: 50,
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f4f4f4',
+      marginLeft: 'auto',
+    },
+    searchIcon: {
+      color: 'black'
+    },
+    cardContainer:{
+      flex: 1,
+      paddingVertical: 20,
+      objectFit: 'contain',
+      alignSelf: 'center',
+      justifyContent: 'center',
+
+    },
+    actionsContainer: {
+      flex: 1,
+      flexDirection:  'row',
+      paddingVertical: 10,
+      backgroundColor: '#ffffff',
+      justifyContent: 'space-evenly'
+    },
+    actionButtonContainer:{
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    actionButton: {
+      backgroundColor: '#f4f4f4',
+      height: 60,
+      width: 60,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 30,
+    },
+    actionText: {
+      padding: 5,
+      fontSize: 21,
+    },
+    transactionsContainer: {
+      flexDirection: 'column',
+      paddingTop: 20,
+      paddingBottom: 20,
+
+    },
+    transctionHeader: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    transactionHeaderText: {
+      fontSize: 26,
+    },
+    sellAllText: {
+      fontSize: 20,
+      color: '#005fff',
+    },
+    transaction: {
+      flexDirection: 'row',
+      paddingTop: 20,
+      justifyContent: 'space-between',
+    },
+    transactionLogoContainer: {
+      backgroundColor: '#f4f4f4',
+      height: 50,
+      width: 50,
+      objectFit: 'contain',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 30,
+    },
+    transactionLogo: {
+      height: 31,
+      width: 27,
+      objectFit: 'contain',
+    },
+    companyDetails: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    transactionTextContainer: {
+      flexDirection: 'column',
+      paddingHorizontal: 20,
+    },
+    transactionText: {
+      fontSize: 22,
+    },
+    transactionSubText: {
+      fontSize: 15,
+      color: '#f4f4f4f',
+    },
+    debit_creditContainer: {
       alignItems: 'center',
       justifyContent: 'center',
     },
+    debitAmount: {
+      fontSize: 22,
+    },
+    creditAmount: {
+      fontSize: 22,
+      color: '#005fff'
+    }
   })
