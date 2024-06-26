9ece1ee6b199ba17960e57d07af0281efdd56c6a
Mawusi Caleb Elolo <cemawusi@st.ug.edu.gh>
2024-06-26 18:06:41 +0000

    Added settings screen

--- Assignment5/screens/settingsScreen.js
+++ Assignment5/screens/settingsScreen.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Switch, StyleSheet, TouchableOpacity } from 'react-native';
+import settingsStyles from '../styles/settingsPageStyles'
+import settingsPageStyles from '../styles/settingsPageStyles';
 
-function SettingsScreen() {
+const SettingsScreen = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleSwitch = () => setIsDarkTheme(previousState => !previousState);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings Screen</Text>
+    <View style={settingsPageStyles.container}>
+      <TouchableOpacity style={settingsPageStyles.option}>
+        <Text style={settingsPageStyles.text}>Language</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={settingsPageStyles.option}>
+        <Text style={settingsPageStyles.text}>My Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={settingsPageStyles.option}>
+        <Text style={settingsPageStyles.text}>Contact Us</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={settingsPageStyles.option}>
+        <Text style={settingsPageStyles.text}>Change Password</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={settingsPageStyles.option}>
+        <Text style={settingsPageStyles.text}>Privacy Policy</Text>
+      </TouchableOpacity>
+
+      <View style={settingsPageStyles.themeSwitchContainer}>
+        <Text style={settingsPageStyles.themeText}>Theme</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#05e15e" }}
+          thumbColor={isDarkTheme ? "white" : "#f4f3f4"}
+          onValueChange={toggleSwitch}
+          value={isDarkTheme}
+        />
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+ 
+});
 
 export default SettingsScreen;
--- Assignment5/styles/settingsPageStyles.js
+++ Assignment5/styles/settingsPageStyles.js
@@ -1,16 +1,30 @@
-import { SafeAreaView } from "react-native";
-import safeViewAndroid from './styles/androidSafeView';
+import { DarkTheme } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
 
-
-export default function settingsPage() {
-    return(
-    <SafeAreaView style={safeViewAndroid.androidSafeArea}>
-        <View style={homeStyles.container}>
-        <StatusBar style="auto" />
-            <Text>Settings</Text>
-        </View>
-
-    
-    </SafeAreaView>
-    );
-}
+export default settingsStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 50,
+        paddingHorizontal: 20,
+      },
+      option: {
+        paddingVertical: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+      },
+      text: {
+        fontSize: 16,
+        paddingVertical: 10,
+      },
+      themeSwitchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: 75,
+        borderBottomColor: '#ddd',
+      },
+      themeText: {
+        fontSize: 25,
+      },
+})
