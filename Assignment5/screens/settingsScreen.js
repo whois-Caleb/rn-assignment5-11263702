fe51e73ef8c3985638532270f2f56cdf521834db
Mawusi Caleb Elolo <cemawusi@st.ug.edu.gh>
2024-06-26 18:27:55 +0000

    Implemented dark mode for settings screen

--- Assignment5/screens/settingsScreen.js
+++ Assignment5/screens/settingsScreen.js
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
-import { View, Text, Switch, StyleSheet, TouchableOpacity } from 'react-native';
-import settingsStyles from '../styles/settingsPageStyles'
-import settingsPageStyles from '../styles/settingsPageStyles';
+import { View, Text, Switch, StyleSheet, TouchableOpacity, StatusBar } from 'react-native';
+import settingsStyles from '../styles/settingsPageStyles.js';
 
 const SettingsScreen = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  const toggleSwitch = () => setIsDarkTheme(previousState => !previousState);
+  const toggleSwitch = () => {
+    setIsDarkTheme(previousState => !previousState);
+  };
 
   return (
-    <View style={settingsPageStyles.container}>
-      <TouchableOpacity style={settingsPageStyles.option}>
-        <Text style={settingsPageStyles.text}>Language</Text>
+    <View style={[settingsStyles.container, isDarkTheme ? settingsStyles.darkContainer : settingsStyles.lightContainer]}>
+      <StatusBar barStyle={isDarkTheme ? "light-content" : "dark-content"} />
+      <TouchableOpacity style={settingsStyles.option}>
+        <Text style={[settingsStyles.text, isDarkTheme ? settingsStyles.darkText : settingsStyles.lightText]}>Language</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={settingsPageStyles.option}>
-        <Text style={settingsPageStyles.text}>My Profile</Text>
+      <TouchableOpacity style={settingsStyles.option}>
+        <Text style={[settingsStyles.text, isDarkTheme ? settingsStyles.darkText : settingsStyles.lightText]}>My Profile</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={settingsPageStyles.option}>
-        <Text style={settingsPageStyles.text}>Contact Us</Text>
+      <TouchableOpacity style={settingsStyles.option}>
+        <Text style={[settingsStyles.text, isDarkTheme ? settingsStyles.darkText : settingsStyles.lightText]}>Contact Us</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={settingsPageStyles.option}>
-        <Text style={settingsPageStyles.text}>Change Password</Text>
+      <TouchableOpacity style={settingsStyles.option}>
+        <Text style={[settingsStyles.text, isDarkTheme ? settingsStyles.darkText : settingsStyles.lightText]}>Change Password</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={settingsPageStyles.option}>
-        <Text style={settingsPageStyles.text}>Privacy Policy</Text>
+      <TouchableOpacity style={settingsStyles.option}>
+        <Text style={[settingsStyles.text, isDarkTheme ? settingsStyles.darkText : settingsStyles.lightText]}>Privacy Policy</Text>
       </TouchableOpacity>
 
-      <View style={settingsPageStyles.themeSwitchContainer}>
-        <Text style={settingsPageStyles.themeText}>Theme</Text>
+      <View style={settingsStyles.themeSwitchContainer}>
+        <Text style={[settingsStyles.themeText, isDarkTheme ? settingsStyles.darkText : settingsStyles.lightText]}>Theme</Text>
         <Switch
           trackColor={{ false: "#767577", true: "#05e15e" }}
           thumbColor={isDarkTheme ? "white" : "#f4f3f4"}
@@ -38,9 +40,5 @@ const SettingsScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
- 
-});
 
 export default SettingsScreen;
--- Assignment5/styles/settingsPageStyles.js
+++ Assignment5/styles/settingsPageStyles.js
@@ -1,30 +1,40 @@
-import { DarkTheme } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
 
 export default settingsStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingTop: 50,
-        paddingHorizontal: 20,
-      },
-      option: {
-        paddingVertical: 15,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ddd',
-      },
-      text: {
-        fontSize: 16,
-        paddingVertical: 10,
-      },
-      themeSwitchContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingTop: 75,
-        borderBottomColor: '#ddd',
-      },
-      themeText: {
-        fontSize: 25,
-      },
-})
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  lightContainer: {
+    backgroundColor: '#fff',
+  },
+  darkContainer: {
+    backgroundColor: '#333',
+  },
+  option: {
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  text: {
+    fontSize: 16,
+    paddingVertical: 10,
+  },
+  lightText: {
+    color: '#000',
+  },
+  darkText: {
+    color: '#fff',
+  },
+  themeSwitchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 75,
+    borderBottomColor: '#ddd',
+  },
+  themeText: {
+    fontSize: 25,
+  },
+});
